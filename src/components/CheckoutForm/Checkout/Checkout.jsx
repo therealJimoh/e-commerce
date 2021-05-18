9289980423c0c70c954247259cf0188c37ef3849
Abdullah Jimoh <jimoh.abdullah69@gmail.com>
2021-05-18 11:44:40 +0100

    update checkout form

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -11,12 +11,24 @@ import {
 } from "@material-ui/core";
 
 import useStyles from "./CheckoutStyles";
+import AddressForm from "../AddressForm";
+import PaymentForm from "../PaymentForm";
 
 const steps = ['Shipping address', 'Payment details' ];
 
 const Checkout = () => {
     const [activeStep, setActiveStep] = useState(0);
     const classes = useStyles();
+
+    const Confirmation = () => (
+        <div>
+            Confirmation
+        </div>
+    );
+
+    const Form = () => activeStep === 0
+        ? <AddressForm />
+        : <PaymentForm />
 
     return (
         <>
@@ -33,6 +45,7 @@ const Checkout = () => {
                             </Step>
                         ))}
                     </Stepper>
+                    { activeStep === steps.length ? <Confirmation /> : <Form /> }
                 </Paper>
            </main>
         </>
